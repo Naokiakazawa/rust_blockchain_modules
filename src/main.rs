accbbc6b30ba9d801194a032280927d6bab6d07b
Naokiakazawa <naoki9akazawa@gmail.com>
2023-08-10 20:33:33 +0900

    restructuring

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-use sha2::Sha256;
-
-mod hash;
+mod execute;
 
 fn main() {
-    let input_data: String = String::from("hello");
-    let inputs_data: Vec<String> = vec![input_data];
-
-    let output_hash: Vec<u8> = hash::get_hash::<Sha256>(inputs_data).unwrap();
-    let hash_string: String = hash::hex_to_string(&output_hash);
-
-    println!("{:?}", output_hash);
-    println!("{}", hash_string);
+    execute::calc_hash::execute_hash();
 }
