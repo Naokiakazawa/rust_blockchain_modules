bc9bc3a17d82b6ade325bd0192396934cb3e89db
Naokiakazawa <naoki9akazawa@gmail.com>
2023-08-10 11:11:30 +0900

    sha256 hash generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use sha2::Sha256;
+
 mod hash;
 
 fn main() {
-    println!("Hello, world!");
+    let input_data: String = String::from("hello");
+    let inputs_data: Vec<String> = vec![input_data];
+
+    let output_hash: Vec<u8> = hash::get_hash::<Sha256>(inputs_data).unwrap();
+    let hash_string: String = hash::hex_to_string(&output_hash);
+    println!("{}", format! {"{}", hash_string});
 }
