396e78885b5a2cd5bcbf3508d48cbd0841f17526
Naokiakazawa <naoki9akazawa@gmail.com>
2023-08-10 21:00:39 +0900

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use tracing::{subscriber, Level};
+use tracing_subscriber::FmtSubscriber;
+
 mod execute;
 
 fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     execute::calc_hash::execute_hash();
 }
