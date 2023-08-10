a8a2dc3369315dfecdefad6eb04c4aa382670140
Naokiakazawa <naoki9akazawa@gmail.com>
2023-08-10 20:36:49 +0900

    refactoring

--- modules/src/hash.rs
+++ modules/src/hash.rs
@@ -15,14 +15,3 @@ pub fn get_hash<D: Digest>(inputs: Vec<String>) -> Result<Vec<u8>> {
 
     Ok(output)
 }
-
-pub fn hex_to_string(data: &[u8]) -> String {
-    let mut ret = String::new();
-
-    for d in data {
-        let x = format!("{:02x}", d);
-        ret.push_str(&x);
-    }
-
-    ret
-}
