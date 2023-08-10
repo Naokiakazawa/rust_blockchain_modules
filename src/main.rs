263c8f251e96ddaa002c8d1451e583589bf5c010
Naokiakazawa <naoki9akazawa@gmail.com>
2023-08-10 13:43:20 +0900

    add print methods

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,7 @@ fn main() {
 
     let output_hash: Vec<u8> = hash::get_hash::<Sha256>(inputs_data).unwrap();
     let hash_string: String = hash::hex_to_string(&output_hash);
-    println!("{}", format! {"{}", hash_string});
+
+    println!("{:?}", output_hash);
+    println!("{}", hash_string);
 }
