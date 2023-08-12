15c1fde8c871a630acae917ff8e3a64936fe6175
Naokiakazawa <naoki9akazawa@gmail.com>
2023-08-12 23:55:38 +0900

    refactoring

--- modules/src/pow.rs
+++ modules/src/pow.rs
@@ -1,28 +1,32 @@
 use crate::hash;
 use crate::utils;
+use serde::{Deserialize, Serialize};
 use tracing::{info, trace};
 
+#[derive(Serialize, Deserialize, Clone, Copy)]
 pub struct Block {
-    previous_hash: [u8; 32],
-    nonce: u32,
+    pub block_hash: [u8; 32],
+    pub previous_hash: [u8; 32],
+    pub nonce: u32,
 }
 
 impl Block {
     fn new() -> Self {
         Block {
+            block_hash: [0; 32],
             previous_hash: [0; 32],
             nonce: 0,
         }
     }
 }
 
-pub fn proof_of_work(height: usize) -> Vec<[u8; 32]> {
+pub fn proof_of_work(height: usize) -> Vec<Block> {
     const TARGET: [u8; 32] = [
         0, 0, 7, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0,
     ];
     let mut output_hash_buffer: [u8; 32] = [0; 32];
-    let mut hash_chain: Vec<[u8; 32]> = Vec::new();
+    let mut block_chain: Vec<Block> = Vec::new();
 
     let mut block: Block = Block::new();
 
@@ -36,22 +40,26 @@ pub fn proof_of_work(height: usize) -> Vec<[u8; 32]> {
         hash::get_hash_256::<sha2::Sha256>(&input, &mut output_hash_buffer);
         let block_hash: [u8; 32] = output_hash_buffer;
 
-        if block.nonce % 1000 == 0 {
-            let current_hash = utils::hex_to_string(&block_hash);
-            trace!(block.nonce, current_hash, "Mining",);
+        if block.nonce % 10000 == 0 {
+            let current_hash: String = utils::hex_to_string(&block_hash);
+            let current_nonce: u32 = block.nonce;
+            trace!(current_nonce, current_hash, "Mining",);
         }
 
         if block_hash < TARGET {
             info!("Success mining");
-            hash_chain.push(block_hash);
+            block.block_hash = block_hash;
+            block_chain.push(block);
+            // Initialize next block.
+            block.block_hash = [0; 32];
             block.previous_hash = block_hash;
             block.nonce = 0;
         } else {
             block.nonce += 1;
         }
 
-        if height == hash_chain.len() {
-            break hash_chain;
+        if height == block_chain.len() {
+            break block_chain;
         }
     }
 }
--- src/execute/calc_pow.rs
+++ src/execute/calc_pow.rs
@@ -3,9 +3,10 @@ use modules::utils;
 use tracing::info;
 
 pub fn execute_pow() {
-    let results: Vec<[u8; 32]> = pow::proof_of_work(5);
-    for r in results.into_iter() {
-        let hash: String = utils::hex_to_string(&r);
-        info!(hash, "Mining Result");
+    let blocks: Vec<pow::Block> = pow::proof_of_work(5);
+    for block in blocks.into_iter() {
+        let hash: String = utils::hex_to_string(&block.block_hash);
+        let nonce: u32 = block.nonce;
+        info!(hash, nonce, "Mining Result");
     }
 }
