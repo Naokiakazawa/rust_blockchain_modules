1ec513cc19958bbec763d6365ffc0ed2f824fa0e
Naokiakazawa <naoki9akazawa@gmail.com>
2023-08-12 22:32:44 +0900

    remove useless return

--- modules/src/hash.rs
+++ modules/src/hash.rs
@@ -1,6 +1,6 @@
 use sha2::Digest;
 
-pub fn get_hash_256<D: Digest>(input: &[u8], output: &mut [u8; 32]) -> () {
+pub fn get_hash_256<D: Digest>(input: &[u8], output: &mut [u8; 32]) {
     let mut hasher = D::new();
     hasher.update(input);
     let hash = hasher.finalize();
